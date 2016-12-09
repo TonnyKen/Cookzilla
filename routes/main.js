17d2c7dbc4bb29433c643e542c26f1e20eb368a8
孟猛 <mm8134@nyu.edu>
2016-12-09 03:04:33 -0500

    hahaha

--- routes/main.js
+++ routes/main.js
@@ -1,40 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('../config/default').pool;
+var session = require('client-sessions');
 
 router.get('/', function (req, res) {
-  console.log('Currently get /main... ');
+  var sess = req.session.uid;
+  console.log(sess);
   // get from database top 10 tags... recipes...
   var tag_query = "SELECT T_ID, TNAME FROM TAG NATURAL JOIN (SELECT * FROM ABOUT WHERE RID = 1) AS A;";
-  var recipe_query = "SELECT RID, TITLE, DESCRIPTION, PHOTOS, RV.AR FROM RECIPE NATURAL JOIN (SELECT RID, AVG(RATING) AS AR FROM REVIEW GROUP BY RID ORDER BY AVG(RATING) DESC ) AS RV;";
+  var recipe_query = "SELECT DISTINCT * FROM ((SELECT RID, TITLE, DESCRIPTION, PHOTOS, RV.AR FROM RECIPE NATURAL JOIN (SELECT RID, AVG(RATING) AS AR FROM REVIEW GROUP BY RID ORDER BY AVG(RATING) DESC ) AS RV) UNION ALL (SELECT DISTINCT RID, TITLE, DESCRIPTION, PHOTOS, RV.AR FROM (SELECT RID, AVG(RATING) AS AR FROM REVIEW GROUP BY RID ORDER BY AVG(RATING) DESC ) AS RV NATURAL JOIN RECIPE NATURAL JOIN ABOUT NATURAL JOIN TAG WHERE TITLE LIKE CONCAT('%',(SELECT KEYWORD FROM `HISTORY_USER_SEARCH_KEYWORD` WHERE USER_NAME = 1 limit 1),'%') OR DESCRIPTION LIKE CONCAT('%',(SELECT KEYWORD FROM `HISTORY_USER_SEARCH_KEYWORD` WHERE USER_NAME = 1 limit 1),'%') OR TNAME LIKE CONCAT('%',(SELECT KEYWORD FROM `HISTORY_USER_SEARCH_KEYWORD` WHERE USER_NAME = 1 limit 1),'%'))) AS A";
+  var tags = [];
   pool.getConnection(function(err, connection) {
     // Use the connection
     connection.query(tag_query, function(err, rows) {
       if(err)throw err;
-      var tags = rows;
+      tags = rows;
       connection.query(recipe_query, function(err, rows) {
         if(err)throw err;
-        var recipes = rows;
-        if(req.session.uid === 'undefined' ||!req.session.uid) {
-          var userinfo = null;
-          console.log('Not login in now');
-          res.render('main',{all_tags:tags, all_recipes:recipes, userinfo:userinfo});
-        }
-        else {
-          console.log(req.session.uid);
-          user_query = "SELECT PROFILE,LOGIN_NAME,NICK_NAME FROM USER WHERE USER_NAME='"+req.session.uid +"';";
-          connection.query(user_query, function(err, rows) {
-            if(err)throw err;
-            console.log('Already logined',rows);
-            var userinfo = rows;
-            res.render('main',{all_tags:tags, all_recipes:recipes, userinfo:userinfo});
-          });
-        }
+        recipes = rows;
+        console.log(tags);
+        // console.log(recipes);
+        res.render('main',{all_tags:tags, all_recipes:recipes});
       });
+      // And done with the connection.
       connection.release();
+        // Don't use the connection here, it has been returned to the pool.
     });
   });
   //console.log(tags);
+
 });
 
 module.exports = router;
