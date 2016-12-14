43b2ed50cb7ee6f1ab20e3e8dbcd2d1d1da0bfef
TonnyKen <192314146@qq.com>
2016-12-14 18:00:36 -0500

    Fix a small bug

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('client-sessions');
 var flash = require('connect-flash');
-
+var csrf = require('csurf')
 var routes = require('./routes');
 
 //var settings = require('./settings');
+var csrfProtection = csrf({ cookie: true })
 
 var app = express();
 
@@ -23,10 +22,7 @@ app.set('view engine', 'ejs');
 //app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser('keyboard cat'));
-//app.use(session({ key: 'sid', cookie: { maxAge: 60000 }}));
 app.use(cookieParser());
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
@@ -36,7 +32,6 @@ app.use(session({
   activeDuration: 5 * 60 * 1000,
 }));
 app.use(flash());
-app.use(express.csrf());
 
 app.use(function (req, res, next) {
   res.locals.user = req.session.user;
